9e087dcfc3e409e55706d6ecf23e267fa01ee2c7
Mark Olbert <mark@arcabama.com>
2020-05-17 20:03:21 -0700

    Cleanup. Broke TargetedProperty into two files for readability

---
+++ J4JCommandLine/target/MappingResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace J4JSoftware.CommandLine
+{
+    [Flags]
+    public enum MappingResults
+    {
+        Unbound = 1 << 0,
+        UnsupportedMultiplicity = 1 << 1,
+        NoKeyFound = 1 << 2,
+        ConversionFailed = 1 << 3,
+        ValidationFailed = 1 << 4,
+
+        Success = 0
+    }
+}
