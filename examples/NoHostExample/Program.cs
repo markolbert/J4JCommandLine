17d26818bebbf5c71db0c8089f98652cc86476e4
Mark Olbert <mark@arcabama.com>
2021-09-27 09:19:50 -0700

    Add example that doesn't use IHost

---
+++ examples/NoHostExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using J4JSoftware.Configuration.CommandLine;
+using J4JSoftware.Configuration.J4JCommandLine;
+using Microsoft.Extensions.Configuration;
+
+namespace NoDIExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJ4JCommandLineForWindows( out var options, out _ )
+                .Build();
+
+            options!.Bind<Program, int>(x => Program.IntValue, "i")!
+                .SetDefaultValue(75)
+                .SetDescription("An integer value");
+
+            options.Bind<Program, string>(x => Program.TextValue, "t")!
+                .SetDefaultValue("a cool default")
+                .SetDescription("A string value");
+
+            options.FinishConfiguration();
+
+            var help = new ColorHelpDisplay(new WindowsLexicalElements(), options);
+            help.Display();
+
+            var parsed = config.Get<Program>();
+
+            if (parsed == null)
+            {
+                Console.WriteLine("Parsing failed");
+
+                Environment.ExitCode = -1;
+                return;
+            }
+
+            Console.WriteLine("Parsing succeeded");
+
+            Console.WriteLine($"IntValue is {IntValue}");
+            Console.WriteLine($"TextValue is {TextValue}");
+
+            Console.WriteLine(options.SpuriousValues.Count == 0
+                ? "No unkeyed parameters"
+                : $"Unkeyed parameters: {string.Join(", ", options.SpuriousValues)}");
+        }
+
+        public static int IntValue { get; set; }
+        public static string TextValue { get; set; }
+
+    }
+}
