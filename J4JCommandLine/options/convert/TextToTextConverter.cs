5cd602c9d876cd7cc39b4c138f0bbd45cb49045d
Mark Olbert <mark@arcabama.com>
2022-12-23 15:33:59 -0800

    added text to text "converter"

---
+++ J4JCommandLine/options/convert/TextToTextConverter.cs
@@ -0,0 +1,17 @@
+﻿using J4JSoftware.Logging;
+
+namespace J4JSoftware.Configuration.CommandLine;
+
+public class TextToTextConverter : TextToValue<string>
+{
+    public TextToTextConverter( IJ4JLogger? logger )
+        : base( logger )
+    {
+    }
+
+    protected override bool ConvertTextToValue( string text, out string? result )
+    {
+        result = text;
+        return true;
+    }
+}
