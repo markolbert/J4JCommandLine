0229c8ca9caf1c1211d753ef3763c2f6e8f38155
Mark Olbert <mark@arcabama.com>
2025-03-01 09:56:34 -0800

    replace with support from J4JSoftware.GeneralUtilities

--- J4JCommandLine/logging/ICommandLineLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Microsoft.Extensions.Logging;
-
-namespace J4JSoftware.Configuration.CommandLine;
-
-internal interface ICommandLineLogger : ILogger
-{
-    List<LogEntry> Entries { get; }
-}
--- J4JCommandLine/logging/LogEntry.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using Microsoft.Extensions.Logging;
-
-namespace J4JSoftware.Configuration.CommandLine;
-
-internal record LogEntry( LogLevel Level, Type LoggedType, string? Text, Exception? Exception );
